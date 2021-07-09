ce704013397b6e8991fc0081d20b9a0d8141e2e5
Daniel Spinardi <55671450+danielspinard@users.noreply.github.com>
2021-07-09 17:23:52 -0300

    Debugging fetchApi method and json return

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,13 @@
-let app_window = $(window);
-let scroll_wolds = 25;
-let content = $("#country-content").empty();
-
-app_window.scroll(function() {
-    if(app_window.scrollTop() == $(document).height() - app_window.height()) {
-        scroll_wolds = scroll_wolds + 10;
-    }
-});
-
 fetchApi("/all", "get", function (response) {
     response.forEach((country, index) => {
-        addCardCountry(content, country);
+        console.log(`
+            region: ${country.region}
+            name: ${country.name}
+            nativeName: ${country.nativeName}
+            capital: ${country.capital}
+            population: ${country.population}
+            area: ${country.area}
+            flag: ${country.flag}
+        `);
     });
 });
