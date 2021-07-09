8701b3abeedd90def297701c80054b1674e5f601
Daniel Spinardi <55671450+danielspinard@users.noreply.github.com>
2021-07-09 16:56:40 -0300

    Add fetchApi helper docblock

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -1,7 +1,14 @@
-function fetchApi(uri, method = "GET") {
+/**
+ * @param {String} uri 
+ * @param {String} method 
+ * @param {CallableFunction} callable
+ * @return {any}
+ */
+function fetchApi(uri, method = "GET", callable) {
   $.ajax({
     url: "https://restcountries.eu/rest/v2" + uri,
     method: method
   }).done(function (response) {
+    callable(response);
   });
 }
