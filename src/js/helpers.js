54a283f8cd7cf81c3a83536811f8d20270be94de
Daniel Spinardi <55671450+danielspinard@users.noreply.github.com>
2021-07-12 15:34:45 -0300

    Fix card structure and add data country name

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -22,23 +22,25 @@ function addCardCountry(content, country) {
   }
   
   content.append(`
-    <div class="col col-6 country-col" id="country" data-country="${country.name}">
-      <div class="country-card">
-          <div class="country-flag">
-              <img
-                alt="${country.name} flag"
-                src="${country.flag}"
-              >
-          </div>
+    <div class="col col-6 col-3 col-2 country-col">
+      <div class="custom-col">
+        <div class="country-card" id="country" data-country="${country.name}">
+            <div class="country-flag">
+                <img
+                  alt="${country.name} flag"
+                  src="${country.flag}"
+                >
+            </div>
 
-          <div class="country-info">
-            <h5>
-              ${country.name + ' ' + country.nativeName}
-            </h5>
-            <p>Region: ${country.region}</p>
-            <p>Capital: ${country.capital}</p>
-            <p>Population: ${country.population}</p>
-          </div>
+            <div class="country-info">
+              <h5>
+                ${country.name + ' ' + country.nativeName}
+              </h5>
+              <p>Region: ${country.region}</p>
+              <p>Capital: ${country.capital}</p>
+              <p>Population: ${country.population}</p>
+            </div>
+        </div>
       </div>
     </div>
   `);
